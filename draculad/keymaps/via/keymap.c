26157d14e4c229241d631cf47e0a818076864844
medoix <medoix@shivv.com>
2024-06-22 13:22:59 +1000

    Update draculad via keymap

--- draculad/keymaps/via/keymap.c
+++ draculad/keymaps/via/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                                 KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                                 KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN,
         LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                                 KC_N,             KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
-                                                 KC_MUTE,                                              TG(_ADJ),
+                                                 KC_MUTE,                                              KC_PSCR,
                                         KC_LCTL, LALT_T(KC_BSPC), LT(_MUS,KC_SPC),    LT(_NUM,KC_DEL), LT(_SYMB,KC_ENT), KC_CAPS
     ),
     [_NUM] = LAYOUT(
@@ -165,9 +165,6 @@ static void render_status(void) {
             break;
         case _SYMB:
             oled_write_P(PSTR("Symbols"), false);
-            break;
-        case _ADJ:
-            oled_write_P(PSTR("Adjust "), false);
             break;
         case _MUS:
             oled_write_P(PSTR("Mouse  "), false);
