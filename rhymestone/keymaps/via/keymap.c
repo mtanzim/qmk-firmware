9bd99d022df43c099bce70745945721fbf88300a
medoix <medoix@shivv.com>
2024-06-22 21:11:12 +1000

    Update Rhymestone VIA Rules

--- rhymestone/keymaps/via/keymap.c
+++ rhymestone/keymaps/via/keymap.c
@@ -42,49 +42,49 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ortho_4x10(
   //,---------------------------------------------------------------------------------------------------.
-          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
+		  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-          KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,   KC_ENT,
+		  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,   KC_ENT,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       KC_Z_SF,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSF,
+	   KC_Z_SF,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSF,
   //`---------+---------+---------+---------+---------+---------+---------+---------+---------+---------'
-       KC_LCTL,  KC_LALT,  KC_LGUI,    LOWER,  KC_BSPC,   KC_SPC,    RAISE,  KC_RGUI,   KC_APP,  KC_RCTL
+	   KC_LCTL,  KC_LALT,  KC_LGUI,    LOWER,  KC_BSPC,   KC_SPC,    RAISE,  KC_RGUI,   KC_APP,  KC_RCTL
   //,---------------------------------------------------------------------------------------------------.
   ),
 
   [_LOWER] = LAYOUT_ortho_4x10(
   //,---------------------------------------------------------------------------------------------------.
-         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+		 KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-         KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_SCLN,  KC_QUOT,
+		 KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_SCLN,  KC_QUOT,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       KC_11SF,   KC_F12,   KC_ESC,   KC_TAB,    KANJI,   KC_DEL,  XXXXXXX,  XXXXXXX,  KC_INT1,  KC_GRSF,
+	   KC_11SF,   KC_F12,   KC_ESC,   KC_TAB,    KANJI,   KC_DEL,  XXXXXXX,  XXXXXXX,  KC_INT1,  KC_GRSF,
   //`---------+---------+---------+---------+---------+---------+---------+---------+---------+---------'
-       _______,  _______,  _______,  _______,   KC_DEL,  _______,  _______,  _______,  _______,  _______
+	   _______,  _______,  _______,  _______,   KC_DEL,  _______,  _______,  _______,  _______,  _______
   //,---------------------------------------------------------------------------------------------------.
   ),
 
   [_RAISE] = LAYOUT_ortho_4x10(
   //,---------------------------------------------------------------------------------------------------.
-          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
+		  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       KC_LSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_RSFT,
+	   KC_LSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_RSFT,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MINS,  KC_INT1,  KC_COMM,   KC_DOT,  KC_SLSF,
+	   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MINS,  KC_INT1,  KC_COMM,   KC_DOT,  KC_SLSF,
   //`---------+---------+---------+---------+---------+---------+---------+---------+---------+---------'
-       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+	   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
   //,---------------------------------------------------------------------------------------------------.
   ),
 
   [_ADJUST] = LAYOUT_ortho_4x10(
   //,---------------------------------------------------------------------------------------------------.
-       QK_BOOT,   RGBRST,  AG_NORM,  AG_SWAP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   KC_INS,  KC_PSCR,
+	   QK_BOOT,   RGBRST,  AG_NORM,  AG_SWAP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   KC_INS,  KC_PSCR,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  XXXXXXX,  KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  KC_NUM,
+	   RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  XXXXXXX,  KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  KC_NUM,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  XXXXXXX,  KC_BTN1,  KC_BTN2,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+	   RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  XXXXXXX,  KC_BTN1,  KC_BTN2,  XXXXXXX,  XXXXXXX,  XXXXXXX,
   //`---------+---------+---------+---------+---------+---------+---------+---------+---------+---------'
-       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+	   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
   //,---------------------------------------------------------------------------------------------------.
   )
 };
@@ -116,13 +116,13 @@ const LAYER_DISPLAY_NAME layer_display_name[LAYER_DISPLAY_MAX] = {
 static inline const char* get_leyer_status(void) {
 
   for (uint8_t i = 0; i < LAYER_DISPLAY_MAX; ++i) {
-    if (layer_state == 0 && layer_display_name[i].state == default_layer_state) {
+	if (layer_state == 0 && layer_display_name[i].state == default_layer_state) {
 
-      return layer_display_name[i].name;
-    } else if (layer_state != 0 && layer_display_name[i].state == layer_state) {
+	  return layer_display_name[i].name;
+	} else if (layer_state != 0 && layer_display_name[i].state == layer_state) {
 
-      return layer_display_name[i].name;
-    }
+	  return layer_display_name[i].name;
+	}
   }
 
   return "?";
@@ -132,7 +132,7 @@ static char layer_status_buf[24] = "Layer state ready.\n";
 static inline void update_keymap_status(void) {
 
   snprintf(layer_status_buf, sizeof(layer_status_buf) - 1, "OS:%s Layer:%s\n",
-    keymap_config.swap_lalt_lgui? "win" : "mac", get_leyer_status());
+	keymap_config.swap_lalt_lgui? "win" : "mac", get_leyer_status());
 }
 
 static inline void render_keymap_status(void) {
@@ -154,18 +154,18 @@ static inline void render_status(void) {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
   if (is_keyboard_master())
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+	return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
   return rotation;
 }
 
 bool oled_task_user(void) {
 
   if (is_keyboard_master()) {
-    render_status();
+	render_status();
   } else {
-    render_logo();
+	render_logo();
   }
-    return false;
+	return false;
 }
 
 #else
@@ -180,46 +180,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   bool result = false;
   switch (keycode) {
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-      } else {
-        layer_off(_LOWER);
-      }
+	case LOWER:
+	  if (record->event.pressed) {
+		layer_on(_LOWER);
+	  } else {
+		layer_off(_LOWER);
+	  }
 
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-      } else {
-        layer_off(_RAISE);
-      }
+	  update_tri_layer(_LOWER, _RAISE, _ADJUST);
+	  break;
+	case RAISE:
+	  if (record->event.pressed) {
+		layer_on(_RAISE);
+	  } else {
+		layer_off(_RAISE);
+	  }
 
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      break;
-    case KANJI:
-      if (record->event.pressed) {
-        if (keymap_config.swap_lalt_lgui == false) {
-          register_code(KC_LNG2);
-        } else {
-          register_code16(A(KC_GRV));
-        }
-      } else {
-        unregister_code(KC_LNG2);
-      }
-      break;
-    #ifdef RGBLIGHT_ENABLE
-      case RGBRST:
-          if (record->event.pressed) {
-            eeconfig_update_rgblight_default();
-            rgblight_enable();
-          }
-        break;
-    #endif
-    default:
-      result = true;
-      break;
+	  update_tri_layer(_LOWER, _RAISE, _ADJUST);
+	  break;
+	case KANJI:
+	  if (record->event.pressed) {
+		if (keymap_config.swap_lalt_lgui == false) {
+		  register_code(KC_LNG2);
+		} else {
+		  register_code16(A(KC_GRV));
+		}
+	  } else {
+		unregister_code(KC_LNG2);
+	  }
+	  break;
+	#ifdef RGBLIGHT_ENABLE
+	  case RGBRST:
+		  if (record->event.pressed) {
+			eeconfig_update_rgblight_default();
+			rgblight_enable();
+		  }
+		break;
+	#endif
+	default:
+	  result = true;
+	  break;
   }
 
   UPDATE_KEYMAP_STATUS();
--- rhymestone/keymaps/via/rules.mk
+++ rhymestone/keymaps/via/rules.mk
@@ -1,4 +1,9 @@
 VIA_ENABLE = yes
+MOUSEKEY_ENABLE = yes       # Mouse keys
+TAP_DANCE_ENABLE = no
+
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+OLED_ENABLE = no
 LTO_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
