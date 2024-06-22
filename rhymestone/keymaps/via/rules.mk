930518af00ff7c8de99f2252179f1ea689eafc69
medoix <medoix@shivv.com>
2024-06-22 20:21:19 +1000

    Update Rhymestone VIA Rules

--- rhymestone/keymaps/via/rules.mk
+++ rhymestone/keymaps/via/rules.mk
@@ -1,8 +1,4 @@
-MOUSEKEY_ENABLE = yes       # Mouse keys
-TAP_DANCE_ENABLE = no
-
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-OLED_ENABLE = no
+VIA_ENABLE = yes
 LTO_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
