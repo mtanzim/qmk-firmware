83cceb6c355d9ec32d84752186119fceeb730a40
medoix <medoix@shivv.com>
2024-09-01 13:55:17 +1000

    Update ArtPad Firmware

--- artpad/keymaps/vial/keymap.c
+++ artpad/keymaps/vial/keymap.c
@@ -79,12 +79,12 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_all(
+  [0] = LAYOUT(
       KC_A, KC_B, KC_C, KC_D, KC_E,
       KC_F, KC_G, KC_H, KC_I, KC_J,
       KC_K, KC_L, KC_M, KC_N, KC_O, MO(1)),
 
-  [1] = LAYOUT_all(
+  [1] = LAYOUT(
       KC_P, KC_Q, KC_R, KC_S, KC_T,
       KC_U, KC_V, KC_W, KC_X, RGB_SAI,
       KC_Z, KC_1, KC_2, KC_3, RGB_SAD, KC_TRNS)
