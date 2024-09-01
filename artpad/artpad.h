521cca168e6ddc9a26d8332470f4942c57a940c5
medoix <medoix@shivv.com>
2024-09-01 13:19:50 +1000

    Update ArtPad Firmware

--- artpad/artpad.h
+++ artpad/artpad.h
@@ -16,16 +16,3 @@
 #pragma once
 
 #include "quantum.h"
-
-#define ___ KC_NO
-
-#define LAYOUT_all( \
-  K00, K01, K02, K03, K04, \
-  K10, K11, K12, K13, K14, \
-  K20, K21, K22, K23, K24, K34 \
-) { \
-	{ K00,   K01,   K02,   K03,   K04 }, \
-	{ K10,   K11,   K12,   K13,   K14 }, \
-	{ K20,   K21,   K22,   K23,   K24 }, \
-	{ ___,   ___,   ___,   ___,   K34 } \
-}
