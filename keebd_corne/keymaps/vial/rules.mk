07707d23a9971638cec649f7a8034b2a43615e16
medoix <medoix@shivv.com>
2024-09-15 21:29:44 +1000

    KEEBD Corne Vial RGB

--- keebd_corne/keymaps/vial/rules.mk
+++ keebd_corne/keymaps/vial/rules.mk
@@ -1,10 +1,9 @@
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 VIAL_INSECURE 		= yes
+VIALRGB_ENABLE 		= yes
 LTO_ENABLE          = yes
 
-RGBLIGHT_ENABLE     = yes
-RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = yes
 EXTRAKEY_ENABLE     = no
