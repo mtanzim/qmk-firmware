d1eb26392933e25d85368cd89e427ccc24100123
medoix <medoix@shivv.com>
2024-09-16 07:25:37 +1000

    KEEBD Corne Vial RGB

--- keebd_corne/keymaps/vial/config.h
+++ keebd_corne/keymaps/vial/config.h
@@ -32,23 +32,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define USE_SERIAL_PD2
-#define RGB_DI_PIN B5
-#define DRIVER_LED_TOTAL 54
+#define BACKLIGHT_PIN B5
 
-// #define BACKLIGHT_BREATHING
 #ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_CHRISTMAS
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
-    #define RGBLIGHT_EFFECT_ALTERNATING
-    #define RGBLIGHT_EFFECT_TWINKLE
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_LED_COUNT 54
+#    define RGBLED_SPLIT \
+        { 27, 27 }
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP  10
+#    define RGBLIGHT_SAT_STEP  17
+#    define RGBLIGHT_VAL_STEP  17
 #endif
