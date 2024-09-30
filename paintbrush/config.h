1a8c6951d00aec8b672dffd44183aef9782192f8
medoix <medoix@shivv.com>
2024-10-01 07:33:50 +1000

    PaintBrush update

---
+++ paintbrush/config.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Mike "KemoNine" Crosson
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+/* Board layout */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 4
+#define UNUSED_PINS
+
+/* Prevent jitters in key presses */
+#define DEBOUNCE 5
