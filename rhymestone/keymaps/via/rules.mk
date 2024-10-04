54f83b813f3b57f7fd2b6bcaca4a24e981d4bb01
medoix <medoix@shivv.com>
2024-10-04 10:52:48 +1000

    Rhymestone include OLED files

--- rhymestone/keymaps/via/rules.mk
+++ rhymestone/keymaps/via/rules.mk
@@ -3,3 +3,6 @@ RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 OLED_ENABLE = yes
 VIA_ENABLE = yes
 LTO_ENABLE = yes
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./oled_helper.c \
