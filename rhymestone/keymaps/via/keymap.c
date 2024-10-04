f1f16d9232c871a5dd3eb4b07334871bf3859331
medoix <medoix@shivv.com>
2024-10-04 13:47:58 +1000

    Rhymestone update

--- rhymestone/keymaps/via/keymap.c
+++ rhymestone/keymaps/via/keymap.c
@@ -13,6 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "host.h"
+#include "rgblight.h"
 #include <stdio.h>
 #include <string.h>
 
