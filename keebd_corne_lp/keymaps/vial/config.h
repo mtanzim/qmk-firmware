283c057452bbe80d11df3f6b9de6d4c4dbbcc609
medoix <medoix@shivv.com>
2024-12-10 08:45:26 +1100

    Add KEEBD Corne LP

---
+++ keebd_corne_lp/keymaps/vial/config.h
@@ -0,0 +1,50 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3B, 0x6B, 0xA0, 0x29, 0x80, 0x56, 0xED, 0xD1}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define TAPPING_TERM 180
+
+/* Select hand configuration */
+#define SOFT_SERIAL_PIN D2
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+// The pin connected to the data pin of the LEDs
+#define BACKLIGHT_PIN B5
+#define RGB_DI_PIN B5
+// The number of LEDs connected
+#define DRIVER_LED_TOTAL 54
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_LED_COUNT 54
+#    define RGBLED_SPLIT \
+        { 27, 27 }
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP  10
+#    define RGBLIGHT_SAT_STEP  17
+#    define RGBLIGHT_VAL_STEP  17
+#endif
---
+++ keebd_corne_lp/keymaps/vial/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+VIAL_INSECURE 		= yes
+VIALRGB_ENABLE 		= yes
+RGB_MATRIX_ENABLE	= yes
+RGB_MATRIX_DRIVER 	= ws2812
+LTO_ENABLE          = yes
+
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = yes
+EXTRAKEY_ENABLE     = no
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = no
