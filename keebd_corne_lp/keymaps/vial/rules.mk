ed1aa0da0d9daa7daf0f286b98d762e89d935c27
medoix <medoix@shivv.com>
2025-05-19 12:34:00 +1000

    LP lighting updates

--- keebd_corne_lp/keymaps/vial/rules.mk
+++ keebd_corne_lp/keymaps/vial/rules.mk
@@ -2,7 +2,6 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 VIAL_INSECURE       = yes
 VIALRGB_ENABLE      = yes
-RGBLIGHT_ENABLE     = yes
 LTO_ENABLE          = yes
 OLED_ENABLE         = yes
 QMK_SETTINGS        = no
